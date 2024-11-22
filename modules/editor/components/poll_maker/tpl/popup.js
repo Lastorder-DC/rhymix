690ba43271ec79b243eda06dfaf4828ea16af24c
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-11-22 09:31:27 +0900

    TEMP: allow delete all options

--- modules/editor/components/poll_maker/tpl/popup.js
+++ modules/editor/components/poll_maker/tpl/popup.js
@@ -70,7 +70,7 @@ jQuery(function($){
 
 		$tr = $(this).prevAll('div').children('table').find('>tbody>tr:last');
 		match = $tr.find('td>input').attr('name').match(/item_(\d+)_(\d+)/);
-		if(!match || match[2] == 2) return;
+		if(!match) return;
 
 		$tr.remove();
 
